c63a575111ac4f37e683cc37db85079ed155e894
Frebla <32418668+Frebla@users.noreply.github.com>
2023-03-01 13:09:30 +0300

    Add files via upload

---
+++ wwwroot/js/Calculator.js
@@ -0,0 +1,69 @@
+﻿$(function () {
+    const app = Vue.createApp({
+        data() {
+            return {
+                SampleList: [],
+                PlasticList: [],
+                DifficultyList: [],
+                selectedType: 1,
+                selectedPlastic: 1,
+                difficult: 1,
+                sizeL: 18,
+                sizeW: 0,
+                sizeD: 0,
+                weightOne: 0,
+                quantity: 1,
+                totalAll: "0",
+                totalOne: "0"
+            }
+        },
+        mounted() {
+            let self = this;
+            $.get(appPath + 'Home/GetSettings', {}, function (response) {
+                debugger;
+                self.SampleList = response.sampleList;
+                self.PlasticList = response.plasticList;
+                self.DifficultyList = response.difficultyList;
+            });
+        },
+        methods: {
+            calculate() {
+                let dto = {
+                    type: this.selectedType,
+                    plastic: this.selectedPlastic,
+                    difficult: this.difficult,
+                    sizeL: this.sizeL,
+                    sizeW: this.sizeW,
+                    sizeD: this.sizeD,
+                    weightOne: this.weightOne,
+                    quantity: this.quantity
+                };
+                let self = this;
+                $.get(appPath + 'Home/Calculate', dto, function (response) {
+                    self.totalAll = response.priceTotalAll;
+                    self.totalOne = response.priceTotalOne;
+                });
+            },
+            checkPass() {
+                bootbox.prompt({
+                    title: "Пароль:",
+                    inputType:'password',
+                    callback: function (result) {
+                        if (result != null) {
+                            $.post(appPath + 'Home/checkPass', { pass: result }, function (response) {
+                                if (response == true) {
+                                    document.location = appPath + 'Home/Settings';
+                                }
+                                else {
+                                    bootbox.alert('Пароль не верен!');
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        }
+
+    }).mount('#app');
+
+})
---
+++ wwwroot/js/Settings.js
@@ -0,0 +1,49 @@
+﻿$(function () {
+    const app = Vue.createApp({
+        data() {
+            return {
+                SampleList: [],
+                PlasticList: [],
+                DifficultyList: [],
+                selectedType: 1,
+                selectedPlastic: 1,
+                difficult: 1,
+                Password: "",
+                PrinterHourCost: 0
+            }
+        },
+        mounted() {
+            let self = this;
+            $.get(appPath + 'Home/GetSettings', {}, function (response) {
+                self.SampleList = response.sampleList;
+                self.PlasticList = response.plasticList;
+                self.DifficultyList = response.difficultyList;
+                self.PrinterHourCost = response.printerHourCost;
+                self.Password = response.password;
+            });
+        },
+        methods: {
+            back() {
+                debugger;
+                document.location = appPath + 'Home/Calculator';
+            },
+            saveSettings() {
+                bootbox.alert('Save!');
+                debugger;
+                let dto = {
+                    sampleList: this.SampleList,
+                    plasticList: this.PlasticList,
+                    DifficultyList: this.DifficultyList,
+                    printerHourCost: this.PrinterHourCost,
+                    password: this.Password
+                };
+                let self = this;
+                $.post(appPath + 'Home/SaveSettings', dto, function (response) {
+                    document.location = appPath + 'Home/Calculator';
+                });
+            },
+        }
+
+    }).mount('#app');
+
+})
